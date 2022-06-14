1c95ce98e10bd7b0d6241db23deb9dd71a259066
RAMESHWAR MAHALE <RAMESHWAR MAHALE@RAMESHWAR>
2022-06-14 15:30:59 +0530

    First Commit

---
+++ GetDemoApp/src/main/java/com/infy/demo/exception2/GetDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.infy.demo.exception2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetDemoAppApplication.class, args);
+	}
+
+}
---
+++ GetDemoApp/src/test/java/com/infy/demo/exception2/GetDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.demo.exception2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
