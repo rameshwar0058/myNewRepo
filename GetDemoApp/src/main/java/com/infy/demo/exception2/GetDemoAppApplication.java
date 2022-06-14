5cdd0b14c844215cfab18d7af4652573d9153678
RAMESHWAR MAHALE <RAMESHWAR MAHALE@RAMESHWAR>
2022-06-14 16:19:32 +0530

    new push

--- GetDemoApp/src/main/java/com/infy/demo/exception2/GetDemoAppApplication.java
+++ GetDemoApp/src/main/java/com/infy/demo/exception2/GetDemoAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GetDemoAppApplication {
 
 	public static void main(String[] args) { 
-		System.out.println("Hello World");
+		System.out.println("Hello GItHub");
 		SpringApplication.run(GetDemoAppApplication.class, args);
 	}
 
