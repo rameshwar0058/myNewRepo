7de043e585300d51fb1e0107d7dd769469a1f30e
RAMESHWAR MAHALE <RAMESHWAR MAHALE@RAMESHWAR>
2022-06-14 16:15:39 +0530

    New

--- GetDemoApp/src/main/java/com/infy/demo/exception2/GetDemoAppApplication.java
+++ GetDemoApp/src/main/java/com/infy/demo/exception2/GetDemoAppApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GetDemoAppApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
+		System.out.println("Hello World");
 		SpringApplication.run(GetDemoAppApplication.class, args);
 	}
 
